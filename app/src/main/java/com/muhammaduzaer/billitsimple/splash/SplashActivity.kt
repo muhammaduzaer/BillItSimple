ca9c6becbcf445a7bb68bf125d97e3e6b58133a9
muhammaduzair <muhammaduzaer@gmail.com>
2020-12-09 14:11:19 +0500

    Initial Commit.

--- app/src/main/java/com/muhammaduzaer/billitsimple/splash/SplashActivity.kt
+++ app/src/main/java/com/muhammaduzaer/billitsimple/splash/SplashActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 import com.muhammaduzaer.billitsimple.MainActivity
 import com.muhammaduzaer.billitsimple.R
 
@@ -12,15 +13,11 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        Handler().postDelayed({
+        Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }, 1000)
     }
 
-
-
-
-    
 }
 
